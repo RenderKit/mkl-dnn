cd61768e1308d563ef9ac4a051967a65d0a48914
Attila Afra <attila.t.afra@intel.com>
2019-03-18 00:37:31 +0200

    CMake fix for Windows static library

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -24,7 +24,9 @@ set(platform_cmake_included true)
 
 #include("cmake/utils.cmake")
 
-add_definitions(-DMKLDNN_DLL -DMKLDNN_DLL_EXPORTS)
+if(MKLDNN_LIBRARY_TYPE STREQUAL "SHARED")
+    add_definitions(-DMKLDNN_DLL -DMKLDNN_DLL_EXPORTS)
+endif()
 
 # UNIT8_MAX-like macros are a part of the C99 standard and not a part of the
 # C++ standard (see C99 standard 7.18.2 and 7.18.4)
